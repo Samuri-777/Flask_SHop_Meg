3c2696fed34671267654985aef09ca977082f546
Samuri-777 <paskaev.adam@mail.ru>
2021-09-22 00:21:35 +0300

    commit

---
+++ gop_shop/routes.py
@@ -0,0 +1,16 @@
+from gop_shop import app
+from flask import render_template
+
+@app.route("/")
+def index():
+    return render_template('index.html')
+
+
+@app.route("/base")
+def base():
+    return render_template("base.html")
+
+
+@app.route('/blog')
+def blog():
+     return render_template('blog.html')
