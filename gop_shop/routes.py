bac29fec37d8abf8e83f5ebcc0765d394742d8f8
Samuri-777 <paskaev.adam@mail.ru>
2021-09-29 07:47:45 +0300

    add product

--- gop_shop/routes.py
+++ gop_shop/routes.py
@@ -1,7 +1,9 @@
+import os
+from werkzeug.utils import secure_filename
 from gop_shop import app
 from flask import render_template, request
 from gop_shop.models import Product, db
-from PIL import Image
+# from PIL import Image
 
 @app.route("/")
 def index():
@@ -22,11 +24,12 @@ def blog():
 @app.route('/add_product', methods=['GET', 'POST'])
 def add_product():
     if request.method == "POST":
-        file_name = request.files.get('image')
-
         f = request.form
+        file_name = request.files.get('image')
+        filename = secure_filename(file_name.filename)
+        file_name.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
         p = Product(title=f.get('title'), price=f.get('price'), category=f.get('category'), availibility=f.get('availibility'),
-        description=f.get('description'), image=file_name)
+        description=f.get('description'), image=file_name.filename)
         db.session.add(p)
         db.session.commit() 
     return render_template('add_product.html')
